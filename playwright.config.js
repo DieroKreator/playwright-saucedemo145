8ae395cd273e7d11facaf4538f6d37a421c0fb12
DieroKreator <qtechwired@gmail.com>
2025-12-15 19:53:27 -0300

    Update config file

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,16 @@
-module.exports = {
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
     use: {
+        baseURL: 'https://www.saucedemo.com',
         browserName: 'firefox',
         headless: true,
+        launchOptions: {
+            slowMo: 1000 // wait between every action
+        },
         // screenshot: 'only-on-failure',
         // video: 'retain-on-failure',
     },
     reporter: [['html', { outputFolder: 'report', open: 'never' }]],
     timeout: 60000,
-}
+});
