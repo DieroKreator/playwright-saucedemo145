c43f9cb98aa8c43ce6298577d3b4d65fd32997d3
DieroKreator <qtechwired@gmail.com>
2025-12-16 14:12:31 -0300

    Refactor Playwright configuration to set up environment variables for artifacts and improve directory structure

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,42 @@
-const { defineConfig } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test');
+const path = require('path');
+const { compute_run_folder, ensure_subdirs } = require('./utils/path_tools');
+
+// Diretórios onde ficam os artefatos
+const ARTIFACTS_ROOT = path.join(__dirname, 'artifacts') // nome pasta raiz
+const runDir = compute_run_folder(ARTIFACTS_ROOT)
+const { resultsDir, screenshotsDir } = ensure_subdirs(runDir)
+
+// Expõe caminhos de diretórios como variaveis de ambiente
+process.env.RUN_DIR = runDir
+process.env.SCREENSHOTS_DIR = screenshotsDir
 
 module.exports = defineConfig({
+    testDir: '.tests',   // nossos testes estão na pasta tests
+    timeout: 30000,      // 30_000 = 30 segundos
+    fullyParallel: true, // execução em paralelo / multi thread
+    outputDir: resultsDir,
+    testDir: './tests',
     use: {
         baseURL: 'https://www.saucedemo.com',
-        browserName: 'firefox',
         headless: true,
+        screenshot: 'only-on-failure',   // apenas quando der erro
+        video: 'retain-on-failure', // salva o vídeo, se houver erro
+        trace: 'retain-on-failure', // salva o trace, se houver erro
+
+        // outros tipos de timeout
+        actionTimeout: 15000, // timeout se nada estiver acontecendo em 15s
+        navigationTimeout: 20000, // timeout se parar o navegador
+
         launchOptions: {
             slowMo: 0 // wait between every action
-        },
-        // screenshot: 'only-on-failure',
-        // video: 'retain-on-failure',
+        }
     },
-    reporter: [['html', { outputFolder: 'report', open: 'never' }]],
-    timeout: 60000,
+
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] }
+        }
+    ]
 });
