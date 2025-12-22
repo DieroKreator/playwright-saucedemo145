08360ce1174f00627fb3b42919e9d9632b3702d8
DieroKreator <qtechwired@gmail.com>
2025-12-22 11:17:14 -0300

    Add steps file

---
+++ feature/support/steps/login.steps.js
@@ -0,0 +1,7 @@
+// import { Given, When, Then } from '@cucumber/cucumber'
+import { chromium } from 'playwright'
+import LoginPage from '../pages/LoginPage.js'
+import InventoryPage from '../pages/InventoryPage.js'
+
+let browser, context, page
+let loginPage, inventoryPage
