82a26f257f50bd9e4df4003006740661965cfb5e
DieroKreator <qtechwired@gmail.com>
2025-12-22 14:16:44 -0300

    Add click method

--- features/support/pages/LoginPage.js
+++ features/support/pages/LoginPage.js
@@ -10,6 +10,9 @@ export default class LoginPage {
     async login(username, password) {
         await this.page.fill(this.username, username)
         await this.page.fill(this.password, password)
+    }
+
+    async click_on_login_button() {
         await this.page.click(this.loginButton)
     }
 }
