6d2fd9cb9657ad15482b7d4971712252b4174935
DieroKreator <qtechwired@gmail.com>
2025-12-22 15:45:23 -0300

    Add method implementation for steps

--- features/support/steps/login.steps.js
+++ features/support/steps/login.steps.js
@@ -5,3 +5,24 @@ import InventoryPage from '../pages/InventoryPage.js'
 
 let browser, context, page
 let loginPage, inventoryPage
+
+Given('the user is on the login page', async () => {
+    browser = await chromium.launch({ headless: true })
+    context = await browser.newContext()
+    page = await context.newPage()
+    loginPage = new LoginPage(page)
+    inventoryPage = new InventoryPage(page)
+    await page.goto('https://www.saucedemo.com')
+});
+
+When('the user enters a valid {string} and {string}', async (user, password) => {
+    await loginPage.login(user, password)
+});
+
+When('clicks on the login button', async () => {
+    await loginPage.click_on_login_button()
+});
+
+Then('the user is redirected to the {string} page and title is {string}', async (url, title) => {
+    await inventoryPage.verify_page_title(url, title)
+});
