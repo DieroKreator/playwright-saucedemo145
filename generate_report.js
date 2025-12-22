2a82b18b306cae1bad964780cbce27c8421893b5
DieroKreator <qtechwired@gmail.com>
2025-12-22 19:04:30 -0300

    Correction to generate bootstrap report

--- generate_report.js
+++ generate_report.js
@@ -1,9 +1,18 @@
+import reporter from 'cucumber-html-reporter'
+
 const options = {
     theme: 'bootstrap',
-    jsonFile: 'cucmuber-report.json',
+    jsonFile: 'cucumber-report.json',
     output: 'cucumber-report.html',
     reportSuiteAsScenarios: true,
-    launchReport: true
+    scenarioTimestamp: true,
+    launchReport: true,
+    metadata: {
+        "App Version": "1.0.0",
+        "Test Environment": "STAGING",
+        "Browser": "Chrome",
+        "Platform": process.platform
+    },
 }
 
-report.generate(options)
+reporter.generate(options)
