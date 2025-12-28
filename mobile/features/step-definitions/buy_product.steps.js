5272e5544ddf94d7a87f3a2bb7e3144d06cea1e0
DieroKreator <qtechwired@gmail.com>
2025-12-27 23:12:42 -0300

    Add step methods

---
+++ mobile/features/step-definitions/buy_product.steps.js
@@ -0,0 +1,29 @@
+
+
+Given("I am on the Products screen", async () => {
+
+})
+
+When("on {string} I select the product at position {int}", async () => {
+
+})
+
+Then("I verify the {string} and the {string}", async () => {
+
+})
+
+When("I add the product to the cart", async () => {
+
+})
+
+Then("the number 1 is displayed on the cart icon", async () => {
+
+})
+
+When("I click on the cart", async () => {
+
+})
+
+Then("I verify the {string}} and the {string} in the cart", async () => {
+
+})
