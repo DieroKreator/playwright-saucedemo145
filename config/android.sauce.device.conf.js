e0f0ecc56b79df0c8d87cb4cab51d6d5ea32d41f
DieroKreator <qtechwired@gmail.com>
2025-12-27 22:52:13 -0300

    Add json to config files

---
+++ config/android.sauce.device.conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "platformName": "Android",
+    "appium:platformVersion": "13.0",
+    "appium:deviceName": "Samsung_Galaxy_A23_free",
+    "appium:deviceOrientation": "portrait",
+    "appium:appPackage": "com.saucelabs.mydemoapp.android",
+    "appium:appActivity": "com.saucelabs.mydemoapp.android.view.activities.SplashActivity",
+    "appium:automationName": "uiAutomator2",
+    "appium:app": "storage:filename=mda-2.0.1-22.apk",
+    "browserName": "",
+    "appium:ensureWebviewsHavePages": true
+}
---
+++ config/android.sauce.emulator.conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "platformName": "Android",
+    "appium:platformVersion": "13.0",
+    "appium:deviceName": "Android GoogleAPI Emulator",
+    "appium:deviceOrientation": "portrait",
+    "appium:appPackage": "com.saucelabs.mydemoapp.android",
+    "appium:appActivity": "com.saucelabs.mydemoapp.android.view.activities.SplashActivity",
+    "appium:automationName": "uiAutomator2",
+    "appium:app": "storage:filename=mda-2.0.1-22.apk",
+    "browserName": "",
+    "appium:ensureWebviewsHavePages": true
+}
