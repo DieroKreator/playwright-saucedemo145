10b43f1e72bcd3375d80dc2f20ce06228da018aa
DieroKreator <qtechwired@gmail.com>
2025-12-28 12:32:22 -0300

    Add logic to new products.page

--- pages/mobile-app/product.page.js
+++ pages/mobile-app/product.page.js
@@ -1,37 +1,15 @@
-class ProductsPage {
+import BasePage from "./base.page"
 
-    async selectProduct(product) {
-
-        // Ler e Selecionar todos os cartões de produto (productIV)
-        const cards = await $$(
-            '-android uiautomator',
-            'new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/productIV")'
-        )
-
-        // Ler cada cartao dentro do conjunto de cartoes
-        // E selecionar aquele que tiver o nome do produto desejado
-
-        for (let cartao of cards) {
-            // Sobe um nível no elemento (pegue o cartão inteiro)
-            let card = await cartao.$('..')
-
-            // Dentro do card/cartao, pega o nome do produto
-            let product_name = await card.$(
-                '-android uiautomator',
-                //'new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/productTV")'
-                `new UiSelector().text("${product}")`
-            )
-
-            let product_name_txt = await product_name.getText()
-
-            // Compara se é o nome do produto desejado
-            if (product_name_txt == product) {
-                await cartao.click() // Se é, clica nele
-                return // sai do for após encontrar o produto desejado
-            }
-
-        }
+module.exports = class ProductPage extends BasePage {
+    get logo() { // seletor do logo MyDemoApp
+        return $('~App logo and name')
     }
 
+    get section_title() {
+        return $('~title')
+    }
 
-} // UI Automator Viewer --> Android Studio
+    async click_on_product(index) {
+        await $(`android=new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/productIV").instance(${index})`).click()
+    }
+}
